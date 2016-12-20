07e55cf519404d00f3e2112492e5cb4762ac95ab
Renat Zubairov <renat.zubairov@gmail.com>
2016-12-20 16:58:02 +0100

    Started with WSDL parsing sample

---
+++ spec/soap.spec.js
@@ -0,0 +1,24 @@
+var soap = require('soap');
+(function () {
+  'use strict';
+  describe('Given sample WSDL we can ', function () {
+    it('create client with it and proof that it is correctly represented', function (done) {
+      soap.createClient(__dirname + '/data/HelloWorldCXF.wsdl', function (err, client) {
+        expect(err).toBeNull();
+        const description = client.describe();
+
+        const services = Object.keys(description);
+        expect(services).toEqual(['SOAPServiceGreeting', 'SOAPServiceGreeting2']);
+
+        const service = description[services[0]];
+        const ports = Object.keys(service);
+        expect(ports).toEqual(['SoapPortName', 'SoapPortName2']);
+
+        const port = service[ports[0]];
+        //console.log(port);
+
+        done();
+      });
+    });
+  });
+})();
