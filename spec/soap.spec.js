5d00283ec6596772ad722992f4783cdcbb948dfc
Renat Zubairov <renat.zubairov@gmail.com>
2016-12-21 17:11:04 +0100

    Added sample for SAP ByD

--- spec/soap.spec.js
+++ spec/soap.spec.js
@@ -17,5 +17,23 @@ var soap = require('soap');
         done();
       });
     });
+
+    it('parse QueryCustomerIn.wsdl from SAP ByD', function (done) {
+      soap.createClient(__dirname + '/data/sapbyd/QueryCustomerIn.wsdl', function (err, client) {
+        expect(err).toBeNull();
+        const description = client.describe();
+
+        const services = Object.keys(description);
+        expect(services).toEqual(['service']);
+
+        const service = description[services[0]];
+        const ports = Object.keys(service);
+        expect(ports).toEqual(['binding_SOAP12', 'binding']);
+
+        //console.log(service.binding_SOAP12.FindByElements.input.CustomerSelectionByElements);
+        done();
+      });
+    });
+
   });
 })();
