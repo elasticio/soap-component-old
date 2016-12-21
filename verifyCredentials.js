9058d6f2750032f82a8e2ab059e15dcc19ddb395
Renat Zubairov <renat.zubairov@gmail.com>
2016-12-21 15:04:51 +0100

    Improved verify credentials

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -4,5 +4,11 @@ const soap = require('soap');
 // This function will be called by the platform to verify credentials
 module.exports = function verifyCredentials(credentials, cb) {
   console.log('Credentials passed for verification %j', credentials);
-  soap.createClient(credentials.wsdlURI, cb);
+  soap.createClient(credentials.wsdlURI, function(err,result) {
+    if (err) {
+      cb(err, {verified: false});
+    } else {
+      cb(null, {verified: true});
+    }
+  });
 };
