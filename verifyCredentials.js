a7ed80d626738f79128e3274f9deb2c17a23ce11
Renat Zubairov <renat.zubairov@gmail.com>
2017-08-15 13:32:51 +0200

    Improved error handling

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -3,13 +3,13 @@ const soap = require('soap');
 
 // This function will be called by the platform to verify credentials
 module.exports = function verifyCredentials(credentials, cb) {
-  console.log('Credentials passed for verification %j', credentials);
-  soap.createClient(credentials.wsdlURI, function(err) {
-    console.log(err.stack || err);
-    if (err) {
-      cb(err, {verified: false});
-    } else {
-      cb(null, {verified: true});
-    }
-  });
+    console.log('Credentials passed for verification %j', credentials);
+    soap.createClient(credentials.wsdlURI, function (err) {
+        if (err) {
+            console.log(err.stack || err);
+            cb(err, {verified: false});
+        } else {
+            cb(null, {verified: true});
+        }
+    });
 };
