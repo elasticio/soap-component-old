feaaa38e9d430e8c63b4649ad0e34502ffc77351
Renat Zubairov <renat.zubairov@gmail.com>
2017-08-24 15:24:42 +0200

    Fixed tests, added eslint

--- spec/call.spec.js
+++ spec/call.spec.js
@@ -1,13 +1,14 @@
 const assert = require('assert');
 const action = require('../lib/actions/call');
+const path = require('path');
 
 describe('Given complex WSDL we can ', function () {
-    it('list operations', function () {
-        return action.getOperations({
-            wsdlURI: __dirname + '/data/sapbyd/QueryCustomerIn.wsdl'
-        }).then((result) => {
-            assert.equal(Object.keys(result).length, 6);
-            assert.equal(Object.keys(result)[0], "service.binding_SOAP12.FindByIdentification");
-        });
+  it('list operations', function () {
+    return action.getOperations({
+      wsdlURI: path.join(__dirname, '/data/sapbyd/QueryCustomerIn.wsdl')
+    }).then(result => {
+      assert.equal(Object.keys(result).length, 6);
+      assert.equal(Object.keys(result)[0], 'service.binding_SOAP12.FindByIdentification');
     });
+  });
 });
--- spec/soap.spec.js
+++ spec/soap.spec.js
@@ -1,18 +1,18 @@
 const soap = require('soap');
 const assert = require('assert');
-const expect = require('chai').expect;
+const path = require('path');
 
 describe('Given coplex WSDL ', function () {
-    it('parse QueryCustomerIn.wsdl from SAP ByD', function () {
-        return soap.createClientAsync(__dirname + '/data/sapbyd/QueryCustomerIn.wsdl').then((client) => {
-            const description = client.describe();
+  it('parse QueryCustomerIn.wsdl from SAP ByD', function () {
+    return soap.createClientAsync(path.join(__dirname, '/data/sapbyd/QueryCustomerIn.wsdl')).then(client => {
+      const description = client.describe();
 
-            const services = Object.keys(description);
-            assert.deepEqual(services,['service']);
+      const services = Object.keys(description);
+      assert.deepEqual(services, ['service']);
 
-            const service = description[services[0]];
-            const ports = Object.keys(service);
-            assert.deepEqual(ports, ['binding_SOAP12', 'binding']);
-        });
+      const service = description[services[0]];
+      const ports = Object.keys(service);
+      assert.deepEqual(ports, ['binding_SOAP12', 'binding']);
     });
+  });
 });
