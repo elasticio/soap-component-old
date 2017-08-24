e34fee484acc3f153f9be209d6958addf8d9e644
Renat Zubairov <renat.zubairov@gmail.com>
2017-08-24 15:17:19 +0200

    Updated integration tests

--- spec-integration/eio-soap-sample.spec.js
+++ spec-integration/eio-soap-sample.spec.js
@@ -1,41 +1,45 @@
-const soap = require('soap');
 const assert = require('assert');
-const expect = require('chai').expect;
 const action = require('../lib/actions/call');
+const request = require('request-promise-native');
 
-describe('Given complex WSDL we can ', function () {
-    it('list operations', function () {
-        return action.getOperations({
-            wsdlURI: __dirname + '/data/sapbyd/QueryCustomerIn.wsdl'
-        }).then((result) => {
-            assert.equal(Object.keys(result).length, 6);
-            assert.equal(Object.keys(result)[0], "service.binding_SOAP12.FindByIdentification");
-        });
-    });
-});
-
-
-describe('Given sample WSDL', function () {
+describe('Given', function () {
     const wsdlURI = "https://eio-soap-sample.herokuapp.com/ws/countries.wsdl";
-    it('should be able to call operation', function () {
-        return action.process({
-            body: {
-                "name": "Spain"
-            }
-        }, {
-            wsdlURI: wsdlURI,
-            operation: 'CountriesPortService.CountriesPortSoap11.getCountry'
-        });
-    });
 
-    it('should be able to call operation with custom XML', function () {
-        return action.process({
-            body: {
-                _xml: "<getCountryRequest xmlns=\"http://spring.io/guides/gs-producing-web-service\"><name>Spain</name></getCountryRequest>"
-            }
-        }, {
-            wsdlURI: wsdlURI,
-            operation: 'CountriesPortService.CountriesPortSoap11.getCountry'
+    describe('WSDL https://eio-soap-sample.herokuapp.com/ws/countries.wsdl is accessible', function() {
+
+        before(() => request(wsdlURI));
+
+        it('should be able to call operation', function () {
+            return action.process({
+                body: {
+                    "name": "Spain"
+                }
+            }, {
+                wsdlURI: wsdlURI,
+                operation: 'CountriesPortService.CountriesPortSoap11.getCountry'
+            }).then((result) => {
+                assert.ok(result.body);
+                assert.ok(result.body.country);
+                assert.equal(result.body.country.name, "Spain");
+                assert.equal(result.body.country.capital, "Madrid");
+            });
         });
+
+        it('should be able to call operation with custom XML', function () {
+            return action.process({
+                body: {
+                    _xml: "<getCountryRequest xmlns=\"http://spring.io/guides/gs-producing-web-service\"><name>Spain</name></getCountryRequest>"
+                }
+            }, {
+                wsdlURI: wsdlURI,
+                operation: 'CountriesPortService.CountriesPortSoap11.getCountry'
+            }).then((result) => {
+                assert.ok(result.body);
+                assert.ok(result.body.country);
+                assert.equal(result.body.country.name, "Spain");
+                assert.equal(result.body.country.capital, "Madrid");
+            });
+        });
+
     });
 });
---
+++ spec/call.spec.js
@@ -0,0 +1,13 @@
+const assert = require('assert');
+const action = require('../lib/actions/call');
+
+describe('Given complex WSDL we can ', function () {
+    it('list operations', function () {
+        return action.getOperations({
+            wsdlURI: __dirname + '/data/sapbyd/QueryCustomerIn.wsdl'
+        }).then((result) => {
+            assert.equal(Object.keys(result).length, 6);
+            assert.equal(Object.keys(result)[0], "service.binding_SOAP12.FindByIdentification");
+        });
+    });
+});
