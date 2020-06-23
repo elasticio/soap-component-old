df89d6eab4e56f097b26cef9b5f85724c6ca80c7
denis.gladikov <denis.gladikov@elastic.io>
2020-06-23 16:00:10 +0300

    Fix soap service not initialized bug Improve integration tests

--- spec-integration/navmanwireless-sample.spec.js
+++ spec-integration/navmanwireless-sample.spec.js
@@ -1,7 +1,10 @@
 const chai = require('chai');
+const sinon = require('sinon');
+const logger = require('@elastic.io/component-logger')();
 require('dotenv').config();
 
 const { SoapClient } = require('../lib/client');
+const action = require('../lib/actions/call');
 
 const { expect } = chai;
 
@@ -9,8 +12,14 @@ describe('Given', () => {
   describe('WSDL is accessible', () => {
     let cfg;
     let client;
+    let self;
 
     before(async () => {
+      self = {
+        emit: sinon.spy(),
+        logger,
+      };
+
       cfg = {
         wsdlURI: 'https://onlineavl2api-us.navmanwireless.com/onlineavl/api/V3.1/service.asmx?WSDL',
         auth: {
@@ -27,13 +36,16 @@ describe('Given', () => {
     });
 
     it('get services', async () => {
-      const result = client.getServices();
-      expect(result).to.deep.equal(['ServiceSoap', 'ServiceSoap12']);
+      const result = await action.getServices.call(self, cfg);
+      expect(result).to.deep.equal({ ServiceSoap: 'ServiceSoap', ServiceSoap12: 'ServiceSoap12' });
     });
 
     it('get service actions list', async () => {
-      const result = client.getServiceOperations('ServiceSoap12');
-      expect(result.length).to.equal(72);
+      const cfgCopy = JSON.parse(JSON.stringify(cfg));
+      cfgCopy.service = 'ServiceSoap12';
+      const result = await action.getOperations.call(self, cfgCopy);
+      expect(typeof result).to.equal('object');
+      expect(Object.keys(result).length).to.equal(72);
     });
 
     it('get service DoLogin action metadata', async () => {
@@ -44,32 +56,36 @@ describe('Given', () => {
     });
 
     it('call service DoLogin action', async () => {
-      const operation = 'DoLogin';
-      const request = {
-        request: {
-          Session: {
-            // SessionId: '1',
+      const cfgCopy = JSON.parse(JSON.stringify(cfg));
+      cfgCopy.service = 'ServiceSoap12';
+      cfgCopy.operation = 'DoLogin';
+      const message = {
+        body: {
+          request: {
+            Session: {
+              // SessionId: '1',
+              targetNSAlias: 'tns',
+              targetNamespace: 'http://onlineavl2.navmanwireless.com/0907/',
+            },
+            UserCredential: {
+              UserName: cfg.auth.basic.username,
+              Password: cfg.auth.basic.password,
+              // ApplicationID: 'guid|s:string|pattern',
+              // ClientID: 'guid|s:string|pattern',
+              // ClientVersion: 's:string',
+              targetNSAlias: 'tns',
+              targetNamespace: 'http://onlineavl2.navmanwireless.com/0907/',
+            },
+            // "IPAddress": "s:string",
+            // "ClockVerificationUtc": "s:dateTime",
             targetNSAlias: 'tns',
             targetNamespace: 'http://onlineavl2.navmanwireless.com/0907/',
           },
-          UserCredential: {
-            UserName: cfg.auth.basic.username,
-            Password: cfg.auth.basic.password,
-            // ApplicationID: 'guid|s:string|pattern',
-            // ClientID: 'guid|s:string|pattern',
-            // ClientVersion: 's:string',
-            targetNSAlias: 'tns',
-            targetNamespace: 'http://onlineavl2.navmanwireless.com/0907/',
-          },
-          // "IPAddress": "s:string",
-          // "ClockVerificationUtc": "s:dateTime",
-          targetNSAlias: 'tns',
-          targetNamespace: 'http://onlineavl2.navmanwireless.com/0907/',
-        },
+        }
       };
 
       client.setService('ServiceSoap12');
-      const result = await client.callOperation(operation, request);
+      const result = await action.process.call(self, message, cfgCopy);
       expect(result[0].DoLoginResult.OperationStatus).to.equal(true);
     });
 
